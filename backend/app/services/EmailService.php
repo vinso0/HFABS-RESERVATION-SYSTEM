46267741a47c12ba489154e7a87f8a895a3b47d5
vinso0 <arvinsocao2005@gmail.com>
2026-03-28 14:34:08 +0800

    fixing inqury feature

--- backend/app/services/EmailService.php
+++ backend/app/services/EmailService.php
@@ -1,9 +1,7 @@
 <?php
 
 // Load PHPMailer classes
-require_once __DIR__ . '/../../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '/../../../vendor/phpmailer/phpmailer/src/SMTP.php';
-require_once __DIR__ . '/../../../vendor/phpmailer/phpmailer/src/Exception.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
